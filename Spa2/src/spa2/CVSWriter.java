1e92564b6cb10838fdcf1855114344dfa00c27b5
Alex Lourenco <alex@lourenco.software>
2015-10-12 22:43:39 -0700

    Added a CVSWriter placeholder

---
+++ Spa2/src/spa2/CVSWriter.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spa2;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author alex
+ */
+public class CVSWriter {
+    public static void userfile() {
+        ArrayList<String> list = new ArrayList();
+        list.add("This");
+        list.add("is");
+        list.add("a");
+        list.add("test");
+        
+    }
+    
+}
